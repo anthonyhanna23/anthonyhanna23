8a7fcac7a18b8ac2033a63a0b2cb666cc8b4f54a
Anthony Hanna <anthonyhanna@Anthonys-MacBook-Pro-9.local>
2026-02-23 19:17:52 -0500

    Final attempt at V7 routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { Switch, Route } from "wouter";
+// ...existing code...
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { Navbar } from "@/components/Navbar";
 import { Footer } from "@/components/Footer";
-import Home from "@/pages/Home";
-import About from "@/pages/About";
-import Portfolio from "@/pages/Portfolio";
-import Contact from "@/pages/Contact";
-import NotFound from "@/pages/not-found";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
+import NotFound from "./pages/not-found";
 import { useEffect } from "react";
-import { BrowserRouter } from "react-router-dom"; 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function Router() {
   const { pathname } = window.location;
@@ -22,13 +22,15 @@ function Router() {
   }, [pathname]);
 
   return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/portfolio" component={Portfolio} />
-      <Route path="/contact" component={Contact} />
-      <Route component={NotFound} />
-    </Switch>
+    <BrowserRouter basename="/anthonyhanna23">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
@@ -40,7 +42,7 @@ function App() {
           <div className="bg-grain min-h-screen flex flex-col">
             <Navbar />
             <main className="flex-grow">
-              <BrowserRouter basename="/anthonyhanna23" />
+              <Router />
             </main>
             <Footer />
           </div>
@@ -50,5 +52,6 @@ function App() {
     </QueryClientProvider>
   );
 }
+
 
 export default App;
